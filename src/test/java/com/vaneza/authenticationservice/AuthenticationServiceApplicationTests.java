a6cfc7404ea2ef1fa4516b2e7d42a56641364ed4
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-06-06 08:51:18 -0500

    chore: initial commit

---
+++ src/test/java/com/vaneza/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaneza.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
